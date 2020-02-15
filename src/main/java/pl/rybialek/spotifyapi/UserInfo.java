80f38a2b16304ccc66fa49266edb7f28971d1b23
Pawel Rybialek <pawel.rybialek@gmail.com>
2020-02-15 22:25:59 +0100

    obtain a token from spotify

---
+++ src/main/java/pl/rybialek/spotifyapi/UserInfo.java
@@ -0,0 +1,21 @@
+package pl.rybialek.spotifyapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserInfo {
+
+    @GetMapping("/hello")
+    public Principal hello(Principal principal) {
+        return principal;
+    }
+
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+}
